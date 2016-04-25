8212d3c62ce0b460ddbb73dce10c89a80023a94e
Ghomsi Adrien <kemchepatou@gmail.com>
2016-04-25 01:52:21 +0000

    update script

--- server.js
+++ server.js
@@ -1,84 +1,39 @@
-//
-// # SimpleServer
-//
-// A simple chat server using Socket.IO, Express, and Async.
-//
-var http = require('http');
-var path = require('path');
+'use strict';
 
-var async = require('async');
-var socketio = require('socket.io');
 var express = require('express');
+var path = require('path');
+var mongo = require('mongodb').MongoClient;
+var routes = require('./app/routes/index.js');
+var api = require('./app/api/url-shortener.js');
+require('dotenv').config({
+  silent: true
+});
+var app = express();
+mongo.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/url-shortener', function(err, db) {
 
-//
-// ## SimpleServer `SimpleServer(obj)`
-//
-// Creates a new instance of SimpleServer with the following options:
-//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
-//
-var router = express();
-var server = http.createServer(router);
-var io = socketio.listen(server);
+  if (err) {
+    throw new Error('Database failed to connect!');
+  } else {
+    console.log('Successfully connected to MongoDB on port 27017.');
+  }
 
-router.use(express.static(path.resolve(__dirname, 'client')));
-var messages = [];
-var sockets = [];
+  // The format follows as, alias to use for real path, also allows permission to such path.
 
-io.on('connection', function (socket) {
-    messages.forEach(function (data) {
-      socket.emit('message', data);
-    });
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
 
-    sockets.push(socket);
-
-    socket.on('disconnect', function () {
-      sockets.splice(sockets.indexOf(socket), 1);
-      updateRoster();
-    });
-
-    socket.on('message', function (msg) {
-      var text = String(msg || '');
-
-      if (!text)
-        return;
-
-      socket.get('name', function (err, name) {
-        var data = {
-          name: name,
-          text: text
-        };
-
-        broadcast('message', data);
-        messages.push(data);
-      });
-    });
-
-    socket.on('identify', function (name) {
-      socket.set('name', String(name || 'Anonymous'), function (err) {
-        updateRoster();
-      });
-    });
+  db.createCollection("sites", {
+    capped: true,
+    size: 5242880,
+    max: 5000
   });
 
-function updateRoster() {
-  async.map(
-    sockets,
-    function (socket, callback) {
-      socket.get('name', callback);
-    },
-    function (err, names) {
-      broadcast('roster', names);
-    }
-  );
-}
+  routes(app, db);
+  api(app, db);
 
-function broadcast(event, data) {
-  sockets.forEach(function (socket) {
-    socket.emit(event, data);
+  var port = process.env.PORT || 8080;
+  app.listen(port, function() {
+    console.log('Node.js listening on port ' + port);
   });
-}
 
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
 });
